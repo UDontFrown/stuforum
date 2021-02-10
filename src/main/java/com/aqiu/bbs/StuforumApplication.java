c4129ab3a83649575fb84ad5006f5f61ec4f5028
hello <hello@63.com>
2021-02-10 09:44:37 +0800

    Initial commit

---
+++ src/main/java/com/aqiu/bbs/StuforumApplication.java
@@ -0,0 +1,13 @@
+package com.aqiu.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuforumApplication.class, args);
+    }
+
+}
